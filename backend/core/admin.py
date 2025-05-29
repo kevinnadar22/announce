62899cdb6df5e473b45acffe8a715a2244877de8
Kevin <jesikamaraj@gmail.com>
2025-05-30 03:30:09 +0530

    Add PressReleaseAdmin to filter active press releases in admin panel

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import PressRelease, TranslatedText, Ministry, AudienceType, Category
 
+class PressReleaseAdmin(admin.ModelAdmin):
+    list_filter = ('active',)
+
 # Register your models here.
-admin.site.register(PressRelease)
+admin.site.register(PressRelease, PressReleaseAdmin)
 admin.site.register(TranslatedText)
 admin.site.register(Ministry)
 admin.site.register(AudienceType)
