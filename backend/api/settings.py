352acd8320bcdca306279847ec58060000442ea1
Kevin <jesikamaraj@gmail.com>
2025-05-30 18:38:28 +0530

    Update API paths to versioned endpoints and enhance schema with additional fields for ministry, audience type, and category names.

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -51,6 +51,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "https://announce.org.in",
     "https://www.announce.org.in",
+    "http://127.0.0.1:5500",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
